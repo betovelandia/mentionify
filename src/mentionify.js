98e143c3fa9c8c0d9a13a8d2e22c7b980536d8d0
Beto <gvelandia@climate.com>
2015-03-19 19:15:21 -0700

    Plugin Constructors.

--- src/mentionify.js
+++ src/mentionify.js
@@ -1,15 +1,64 @@
 /*
- * 
- * 
+ * Mentionify: Mention people in a textarea.
+ *
  *
  * Copyright (c) 2015 Alberto Velandia
  * Licensed under the MIT license.
  */
 (function ($) {
-  $.fn.mentionify = function () {
-    return this.each(function (i) {
-      // Do something to each selected element.
-      $(this).html('mentionify' + i);
+  'use strict';
+
+  /**
+   * Name of the plugin
+   * @private
+   */
+  var pluginName = 'mentionify',
+
+  /**
+   * Default settings
+   * @private
+   */
+  defaults = {
+    matchCase: true,
+    minChars: 1,
+    keyDelay: 400,
+    userHighlight: true,
+    queryParam: 'key'
+  },
+
+  /**
+   * Mentionify constructor
+   * @param {HTMLElement} element
+   * @param {Object} options
+   * @constructor
+   */
+  function Mentionify(element, options) {
+    // @TODO
+  };
+
+  /**
+   * Plugin constructor
+   * @param {Object|String} options
+   * @returns {JQuery}
+   * @constructor
+   */
+  $.fn.mentionify = function (options) {
+    var instance;
+
+    this.each(function() {
+      instance = $.data(this, 'mentionify');
+
+      if (instance) {
+        // Destroy the plugin instance if exists
+        instance.destroy();
+      }
+
+      // Create the plugin instance
+      instance = new Mentionify(this, options);
+      instance.index = $mentionify.lookup.push(instance) - 1;
+      $.data(this, 'mentionify', instance);
     });
+
+    return this;
   };
 }(jQuery));
