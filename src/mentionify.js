432a4d0610776a9b98a4ecf61e3b6a9e18a0b1e2
Alberto Velandia <albertovelandia@me.com>
2015-08-13 14:05:53 -0700

    Methods structure - Initial work.

--- src/mentionify.js
+++ src/mentionify.js
@@ -6,7 +6,7 @@
  * Licensed under the MIT license.
  */
 (function ($) {
-  'use strict';
+  "use strict";
 
   /**
    * Default settings
@@ -17,10 +17,15 @@
     minChars: 1,
     keyDelay: 400,
     userHighlight: true,
-    queryParam: 'key'
-  },
+    showAvatar: true,
+    queryParam: "key"
+  };
 
-  keyCodes = {
+  /**
+   * Keycodes.
+   * @private
+   */
+  var keyCodes = {
     backspace: 8,
     shift: 16,
     enter: 13,
@@ -31,6 +36,62 @@
     del: 46,
     up: 38
   };
+
+  var $matches = $("ul#mentionify-matches");
+
+  /**
+   * Hides the UL and removes the candidates.
+   * @private
+   */
+  function hideMatches() {
+    $matches.remove('li');
+    $matches.hide();
+  }
+
+  /**
+   * Once the user types after @ grab the string that follows.
+   * @returns {Boolean|String} the param or false if the param length is not
+   * enough based on the plugin's configuration,
+   * @private
+   */
+  function findQuery(text) {
+    var param;
+
+    if (param.length >== settings.minChars) {
+      return param;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Fetch the matches based on the query entered by the user.
+   * @private
+   */
+  function fetchMatches(param, callback) {
+    $.getJSON(buildUrl(param), callback);
+  }
+
+  // @TODO: eval url lib.
+  function buildUrl(param) {
+    return (settings.url + "?" + settings.queryParam + param + settings.extraParams);
+  }
+
+  /**
+   * Appends the list items of each user.
+   * @private
+   */
+  function processMatches(data) {
+    $matches.remove('li');
+    $(data).each(function (key, user) {
+      var $li = $('<li>' + user.nickname + '</li>');
+      if (settings.showAvatar) {
+        $li.append('<img src="' + user.avatar + '"/>');
+      }
+      $matches.append($li);
+      $li.hover(onMatchHover);
+    });
+  }
 
   /**
    * Mentionify constructor
@@ -50,6 +111,7 @@
    */
   Mentionify.prototype.init = function() {
     this.$element.keyUp(this.onKeyUp);
+    this.$element.append($matches);
     return this;
   };
 
@@ -60,10 +122,14 @@
    * @public
    */
   Mentionify.prototype.onKeyUp = function(event) {
-    var allowedKeys = [keyCodes.enter, keyCodes.shift, keyCodes.ctrl];
+    // @TODO: Check last character function need.
+    var text = this.value,
+      isMentioning = text.charAt(text.length -1) === "@";
 
-    if(allowedKeys.indexOf(event.keyCode) > -1) {
-      // @TODO
+    if (isMentioning && findQuery(text)) {
+      $.getJSON(getUrl(findQuery(text)), processMatches);
+    } else  {
+      hideMatches();
     }
   };
 
@@ -77,17 +143,17 @@
     var instance;
 
     this.each(function() {
-      instance = $.data(this, 'mentionify');
+      instance = $.data(this, "mentionify");
 
+      // Destroy the plugin instance if exists.
       if (instance) {
-        // Destroy the plugin instance if exists
         instance.destroy();
       }
 
-      // Create the plugin instance
+      // Create the plugin instance.
       instance = new Mentionify(this, options);
       instance.index = $.mentionify.lookup.push(instance) - 1;
-      $.data(this, 'mentionify', instance);
+      $.data(this, "mentionify", instance);
     });
 
     return this;
